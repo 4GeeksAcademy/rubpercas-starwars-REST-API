7603325c40899bd25f48955cfe42c42f7112b026
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2025-01-23 10:05:28 +0000

    added vehicle model

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    favorites = db.relationship("Favorite", back_populates="user")
+    favorites = db.relationship("Favorite", back_populates="user", lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -24,6 +24,8 @@ class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250))
+    gender = db.Column(db.String(20), nullable=False)
+    hair_color = db.Column(db.String(20), nullable=False)
 
     favorites = db.relationship("Favorite", back_populates="character")
 
@@ -31,7 +33,9 @@ class Character(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "description": self.description
+            "description": self.description,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
         }
 
 class Planet(db.Model):
@@ -39,6 +43,7 @@ class Planet(db.Model):
     name = db.Column(db.String(250), nullable=False)
     climate = db.Column(db.String(250))
     terrain = db.Column(db.String(250))
+    population = db.Column(db.Integer)
  
     favorites = db.relationship("Favorite", back_populates="planet")
 
@@ -47,23 +52,47 @@ class Planet(db.Model):
             "id": self.id,
             "name": self.name,
             "climate": self.climate,
-            "terrain": self.terrain
+            "terrain": self.terrain,
+            "population": self.population
         }
+    
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    cargo_capacity = db.Column(db.Integer)
+    length = db.Column(db.Float)
+
+    favorites = db.relationship("Favorite", back_populates="vehicle")
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "cargo_capacity": self.cargo_capacity,
+            "length": self.length,
+        }
+    
 
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=True)
 
     user = db.relationship("User", back_populates="favorites")
     character = db.relationship("Character", back_populates="favorites", uselist=False)
     planet = db.relationship("Planet", back_populates="favorites", uselist=False)
+    vehicle = db.relationship("Vehicle", back_populates="favorites")
 
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
             "character_id": self.character_id,
-            "planet_id": self.planet_id
+            "planet_id": self.planet_id,
+            "vehicle_id": self.vehicle
         }
